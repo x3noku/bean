ddb270b68a5e00dba54e5daf82a77d6b85c8287c
x3noku <x3noku@gmail.com>
2025-06-14 20:17:32 +0600

    chore: update rename script

--- rename.sh
+++ rename.sh
@@ -1,4 +1,23 @@
+#!/bin/bash
+
 name=$1
 
-grep -rl "bean" --exclude-dir=.git | xargs sed -i "s/bean/${name}/g"
-grep -rl "Bean" --exclude-dir=.git | xargs sed -i "s/Bean/${name^}/g"
+# Find and replace lowercase "bean"
+find . -type f -not -path '*/.git/*' -exec grep -l "bean" {} + | while read -r file; do
+    if [[ "$(uname)" = "Darwin" ]]; then
+        sed -i '' "s/bean/${name}/g" "$file" # macOS requires empty extension for -i flag
+    else
+        sed -i "s/bean/${name}/g" "$file"
+    fi
+done
+
+# Find and replace uppercase "Bean"
+find . -type f -not -path '*/.git/*' -exec grep -l "Bean" {} + | while read -r file; do
+    capitalized_name=$(echo "${name^}")
+
+    if [[ "$(uname)" = "Darwin" ]]; then
+        sed -i '' "s/Bean/${capitalized_name}/g" "$file" # macOS requires empty extension for -i flag
+    else
+        sed -i "s/Bean/${capitalized_name}/g" "$file"
+    fi
+done
